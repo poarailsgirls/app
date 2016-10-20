ab936216bd33741736131dfe63043f6ba5c7b14e
tutabeier <lucas.beier@gmail.com>
2016-10-19 22:14:49 -0200

    Adding markers

--- js/map.js
+++ js/map.js
@@ -3,23 +3,48 @@ var global;
 var ondeEstou;
 var map;
 
+var locations = [];
+
 function initMap() {
   tecnopuc = new google.maps.LatLng(-30.059601, -51.171705);
+  locations.push(['Tecnopuc', tecnopuc]);
   global = new google.maps.LatLng(-30.059857, -51.170198);
+  locations.push(['Global', global]);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      ondeEstou = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      locations.push(['Você', ondeEstou]);
+      map.setCenter(ondeEstou);
+    }, function() {});
+  }
+
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 18,
     center: tecnopuc,
-    mapTypeControl: false
   });
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      ondeEstou = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      map.setCenter(pos);
-    }, function() {});
+  var infowindow = new google.maps.InfoWindow();
+
+  for (i = 0; i < locations.length; i++) {
+    console.log(locations[i]);
+    var marker = new google.maps.Marker({
+      position: locations[i][1],
+      map: map,
+      title: locations[i][0]
+    });
+    // process multiple info windows
+    (function(marker, i) {
+      // add click event
+      google.maps.event.addListener(marker, 'click', function() {
+        if (infowindow) {
+          infowindow.close();
+        }
+        infowindow = new google.maps.InfoWindow({
+          content: locations[i][0]
+        });
+        infowindow.open(map, marker);
+      });
+    })(marker, i);
   }
 }
 
